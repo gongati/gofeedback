25d99a136b1f11c5e2989e81973cb20a3facef17
Patan Kalebu <kalebupatan7@gmail.com>
2020-03-25 23:35:03 +0530

    Added GFFeedbackModel File

---
+++ gofeedback/gofeedback/Models/GFFeedbackModel.swift
@@ -0,0 +1,18 @@
+//
+//  GFFeedbackModel.swift
+//  gofeedback
+//
+//  Created by OMNIADMIN on 25/03/20.
+//  Copyright © 2020 Vishnu. All rights reserved.
+//
+
+import Foundation
+
+struct FeedbackModel {
+    
+    let howWeAreDoing: String
+    let whatWeAreDoingGreat: String
+    let whatCanWeDoBetter: String
+    let comments: String
+    let rating: Double
+}
